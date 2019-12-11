e9eb58293d8a91211781d07320feafa5c0a95a5a
Ligang <tamader0817@gmail.com>
2019-12-10 16:22:33 -0800

    Initial Commit

---
+++ Recipe-CICCC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipe-CICCC
+//
+//  Created by Argus Chen on 2019-12-10.
+//  Copyright © 2019 Argus Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Recipe-CICCCTests/Recipe_CICCCTests.swift
@@ -0,0 +1,34 @@
+//
+//  Recipe_CICCCTests.swift
+//  Recipe-CICCCTests
+//
+//  Created by Argus Chen on 2019-12-10.
+//  Copyright © 2019 Argus Chen. All rights reserved.
+//
+
+import XCTest
+@testable import Recipe_CICCC
+
+class Recipe_CICCCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
