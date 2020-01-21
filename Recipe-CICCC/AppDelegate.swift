706a329c6615309dc7de8de4b215f1d7ee272c7d
Li-Gang <tamader0817@gmail.com>
2020-01-21 01:09:56 -0800

    Update AppDelegate.swift

--- Recipe-CICCC/AppDelegate.swift
+++ Recipe-CICCC/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
         
         
-        
+        //fetch test3
     }
 
 
