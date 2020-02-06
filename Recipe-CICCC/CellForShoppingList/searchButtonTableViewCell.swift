3795084e8086f4527c572508139b75d82c85bfdd
Shihomi Kitajima <shihomi0104@icloud.com>
2020-02-05 22:03:48 -0800

    complete goggle map api and working on shoppingList

---
+++ Recipe-CICCC/CellForShoppingList/searchButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  searchButtonTableViewCell.swift
+//  Recipe-CICCC
+//
+//  Created by 北島　志帆美 on 2020-02-05.
+//  Copyright © 2020 Argus Chen. All rights reserved.
+//
+
+import UIKit
+
+class searchButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
