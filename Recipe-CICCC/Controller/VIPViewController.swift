54ce523d1cda2c27864511b6272986de08773205
fangyilai <roselai1993@gmail.com>
2020-02-20 20:34:54 -0800

    add vip controller

---
+++ Recipe-CICCC/Controller/VIPViewController.swift
@@ -0,0 +1,32 @@
+//
+//  VIPviewController.swift
+//  Recipe-CICCC
+//
+//  Created by fangyilai on 2020-02-20.
+//  Copyright © 2020 Argus Chen. All rights reserved.
+//
+
+import UIKit
+
+class VIPviewController: UIViewController {
+
+    @IBOutlet
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
