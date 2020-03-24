a944fd93675d2611b27d0dae3e6d791b28f49019
fangyilai <roselai1993@gmail.com>
2020-03-23 17:31:52 -0700

    pass ingredients and instructions to recipe detail page

--- Recipe-CICCC/ListForRecipePage/howToCookList.swift
+++ Recipe-CICCC/ListForRecipePage/howToCookList.swift
@@ -17,11 +17,11 @@ class howToCookList {
             let row2Item = HowToCookTableViewCell()
             let row3Item = HowToCookTableViewCell()
             
-            row0Item.howToCook = "Position a rack in the lower third of the oven and heat the oven to 350°F. Butter and then flour the bottom and sides of a 6-cup (8-1/2 x 4-1/2-inch or 9 x 5-inch) loaf pan, tapping out any excess flour."
-            row1Item.howToCook = "In a large bowl, combine the flour, sugar, baking powder, and salt. Whisk until well blended. Stir in the chocolate chips and dried cherries."
-            row2Item.howToCook = "Position a rack in the lower third of the oven and heat the oven to 350°F. Butter and then flour the bottom and sides of a 6-cup (8-1/2 x 4-1/2-inch or 9 x 5-inch) loaf pan, tapping out any excess flour."
-            row3Item.howToCook = "difsaj;ghjo;aihgjo:iahgd"
-            
+//            row0Item.howToCook = "Position a rack in the lower third of the oven and heat the oven to 350°F. Butter and then flour the bottom and sides of a 6-cup (8-1/2 x 4-1/2-inch or 9 x 5-inch) loaf pan, tapping out any excess flour."
+//            row1Item.howToCook = "In a large bowl, combine the flour, sugar, baking powder, and salt. Whisk until well blended. Stir in the chocolate chips and dried cherries."
+//            row2Item.howToCook = "Position a rack in the lower third of the oven and heat the oven to 350°F. Butter and then flour the bottom and sides of a 6-cup (8-1/2 x 4-1/2-inch or 9 x 5-inch) loaf pan, tapping out any excess flour."
+//            row3Item.howToCook = "difsaj;ghjo;aihgjo:iahgd"
+//            
             howToCookList.append(row0Item)
             howToCookList.append(row1Item)
             howToCookList.append(row2Item)
