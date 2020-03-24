ea93f8f733e9037b322d31e03637cc8dfd1688ba
fangyilai <roselai1993@gmail.com>
2020-03-23 18:05:23 -0700

    pass like button number to recipe detail page

--- Recipe-CICCC/ListForRecipePage/userPageViewController.swift
+++ Recipe-CICCC/ListForRecipePage/userPageViewController.swift
@@ -140,7 +140,7 @@ extension userPageViewController: ReloadDataDelegate{
 
 extension userPageViewController: CollectionViewInsideUserTableView{
     func cellTaped(data: IndexPath) {
-        
+
         let storyboard = UIStoryboard(name: "RecipeDetail", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "detailvc") as! RecipeDetailViewController
         vc.userProfile = true
