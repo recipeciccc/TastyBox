94f6a73cda85f2b83e14bb46aa359ce6dda8ba0b
Shihomi Kitajima <shihomi0104@icloud.com>
2020-04-09 01:12:29 -0700

    trying to show the name in follower and following page

--- Recipe-CICCC/Controller/Profile/FollowerViewController.swift
+++ Recipe-CICCC/Controller/Profile/FollowerViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class FollowerViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var followersIDs: [String] = []
+    var followers:[User] = []
+
+    let userDataManager = UserdataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.searchBar.delegate = self as? UISearchBarDelegate
+        self.tableView.dataSource = self
+        userDataManager.delegateFollowerFollowing = self
+        
+        let parentVC = self.parent as! followerFollowingPageViewController
+        followersIDs = parentVC.followersID
+        userDataManager.getFollowersFollowings(IDs: self.followingsID, followerOrFollowing: "follower")
     }
     
 
@@ -27,4 +42,37 @@ class FollowerViewController: UIViewController {
     }
     */
 
+}
+
+
+extension FollowerViewController: FolllowingFollowerDelegate {
+    func passFollowerFollowing(followingsIDs: [String], followersIDs: [String]) {
+        
+    }
+    
+    func assignFollowersFollowings(users: [User]) {
+        self.followers = users
+    }
+    
+}
+
+
+extension FollowerViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return followers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCell(withIdentifier: "followerUser") as? follwerUserTableViewCell)!
+        
+        if followings.isEmpty {
+            cell.userNameLabel.text = "no follower"
+        } else {
+            cell.userNameLabel.text = followers[indexPath.row].name
+        }
+        
+        return cell
+    }
+    
+    
 }
--- Recipe-CICCC/Controller/Profile/FollowingViewController.swift
+++ Recipe-CICCC/Controller/Profile/FollowingViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class FollowingViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+       @IBOutlet weak var tableView: UITableView!
+    
+    var followingsID:[String] = []
+    var followings:[User] = []
 
+    let userDataManager = UserdataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.searchBar.delegate = self as? UISearchBarDelegate
+        self.tableView.dataSource = self
+        userDataManager.delegateFollowerFollowing = self
+        
+        let parentVC = self.parent as! followerFollowingPageViewController
+        followingsID = parentVC.followingsID
+        userDataManager.getFollowersFollowings(IDs: self.followingsID, followerOrFollowing: "following")
     }
     
 
@@ -27,4 +43,36 @@ class FollowingViewController: UIViewController {
     }
     */
 
+}
+
+extension FollowingViewController: FolllowingFollowerDelegate {
+    func passFollowerFollowing(followingsIDs: [String], followersIDs: [String]) {
+        
+    }
+    
+    func assignFollowersFollowings(users: [User]) {
+        self.followings = users
+    }
+    
+}
+
+
+extension FollowingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return followings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCell(withIdentifier: "followerUser") as? follwerUserTableViewCell )!
+        
+        if followings.isEmpty {
+            cell.userNameLabel.text = "no following"
+        } else {
+            cell.userNameLabel.text = followings[indexPath.row].name
+        }
+        
+        return cell
+    }
+    
+    
 }
