d17d1cb85635f9f36f80f492c80e614c7f759074
Shihomi Kitajima <shihomi0104@icloud.com>
2020-04-19 18:32:10 -0700

    complete recipeViewController

---
+++ RecipeViewControllerDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeViewControllerDataManager.swift
+//  Recipe-CICCC
+//
+//  Created by 北島　志帆美 on 2020-04-19.
+//  Copyright © 2020 Argus Chen. All rights reserved.
+//
+
+import Foundation
