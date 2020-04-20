026eb65046ee3e6c1605671b2f4dcbd3585512e3
Shihomi Kitajima <shihomi0104@icloud.com>
2020-04-20 03:50:39 -0700

    start to work on searching page

---
+++ Recipe-CICCC/Controller/searching/SearchingGenreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchingGenreViewController.swift
+//  
+//
+//  Created by 北島　志帆美 on 2020-04-20.
+//
+
+import UIKit
+
+class SearchingGenreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
