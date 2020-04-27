df6787372d21df0b9d6f2e966642c3a970eb3d3f
Shihomi Kitajima <shihomi0104@icloud.com>
2020-04-26 23:54:28 -0700

    before import ML kit Pods

---
+++ Recipe-CICCC/Controller/settings/SettingNotificationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingNotificationViewController.swift
+//  Recipe-CICCC
+//
+//  Created by 北島　志帆美 on 2020-04-26.
+//  Copyright © 2020 Argus Chen. All rights reserved.
+//
+
+import UIKit
+
+class SettingNotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipe-CICCC/StoryBoard/SettingsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsTableViewCell.swift
+//  Recipe-CICCC
+//
+//  Created by 北島　志帆美 on 2020-04-26.
+//  Copyright © 2020 Argus Chen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
