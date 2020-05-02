55f91771ad161f4b729f23e5a28990ac36d40f1a
fangyilai <roselai1993@gmail.com>
2020-05-01 19:11:44 -0700

    change ingredient title color

--- Recipe-CICCC/Views/CellForIngredientRecipe/IngredientTitleCollectionViewCell.swift
+++ Recipe-CICCC/Views/CellForIngredientRecipe/IngredientTitleCollectionViewCell.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class IngredientTitleCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var ingredient: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var titleView: UIView!
     
     override func awakeFromNib() {
-        ingredient.textAlignment = .center
+        titleLabel.textAlignment = .center
     }
+    
+    func focusCell(active: Bool) {
+           let color = active ? #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.728063643, green: 1, blue: 0.9948994517, alpha: 1)
+           titleView.backgroundColor = color
+           let labelColor = active ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0, green: 0.5880746245, blue: 1, alpha: 1)
+           titleLabel.textColor = labelColor
+       }
     
 }
