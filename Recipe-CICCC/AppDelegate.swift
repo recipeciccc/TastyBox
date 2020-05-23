f7cc977b207f9bc9af318a40e05845ab5e7ba0bb
Shihomi Kitajima <shihomi0104@icloud.com>
2020-05-23 14:25:15 -0700

    chage the apple sign in button

--- Recipe-CICCC/AppDelegate.swift
+++ Recipe-CICCC/AppDelegate.swift
@@ -42,27 +42,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         UINavigationBar.appearance().barTintColor = UIColor.white
     
         
-//        if #available(iOS 10.0, *) {
-//            // For iOS 10 display notification (sent via APNS)
-//            UNUserNotificationCenter.current().delegate = self
-//            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//            UNUserNotificationCenter.current().requestAuthorization(
-//            options: authOptions,
-//            completionHandler: {_, _ in })
-//            // For iOS 10 data message (sent via FCM
-//            Messaging.messaging().delegate = self
-//        } else {
-//            let settings: UIUserNotificationSettings =
-//            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-//              application.registerUserNotificationSettings(settings)
-//        }
-//
-//        application.registerForRemoteNotifications()
-//
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            Messaging.messaging().delegate = self
+        } else {
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+              application.registerUserNotificationSettings(settings)
+        }
 
-//        NotificationCenter.default.addObserver(forName: statusBarTappedNotification.name, object: .none, queue: .none) { _ in
-//                  self.navigationController.setNavigationBarHidden(false, animated: false)
-//              }
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
         
          FirebaseApp.configure()
          
